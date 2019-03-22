475c3ab0eef3da1c06ebdae3441b65d78d40d098
Greg Trihus <greg_trihus@sil.org>
2019-03-22 15:22:34 -0500

    Updated schema and the resulting .json and .sql files.

---
+++ modeling/modelCreateSql.xsl
@@ -0,0 +1,130 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+	xmlns:rng="http://relaxng.org/ns/structure/1.0"  version="1.0">
+	
+	<xsl:output  method="text"/>
+	<xsl:variable name="uc">ABCDEFGHIJKLMNOPQRSTUVWXYZ</xsl:variable>
+	<xsl:variable name="lc">abcdefghijklmnopqrstuvwxyz</xsl:variable>
+	
+	<xsl:template match="rng:element|rng:attribute">
+		<xsl:choose>
+			<xsl:when test="parent::*[local-name() = 'zeroOrMore' or local-name() = 'oneOrMore']/parent::*/@name = 'models' or parent::*/@name = 'models'">
+				<xsl:text>CREATE TABLE </xsl:text>
+				<xsl:value-of select="translate(@name,$uc,$lc)"/>
+				<xsl:text> (&#10;</xsl:text>
+				<xsl:apply-templates/>
+				<xsl:if test="not(descendant::*[@name='relationships'])">
+					<xsl:text>);&#10;</xsl:text>
+				</xsl:if>
+			</xsl:when>
+			<xsl:when test="@name = 'keys' or @name = 'attributes' or @name = 'links' or @name = 'transcriberSettings'">
+				<xsl:apply-templates/>
+			</xsl:when>
+			<xsl:when test="@name = 'relationships'">
+				<xsl:for-each select="descendant::rng:element[rng:element]">
+					<xsl:text>    </xsl:text>
+					<xsl:value-of select="translate(@name,$uc,$lc)"/>
+					<xsl:value-of select="substring('                       ', string-length(@name))"/>
+					<xsl:text>uuid REFERENCES </xsl:text>
+					<xsl:value-of select="descendant::rng:value"/>
+					<xsl:text> (id)</xsl:text>
+					<xsl:text>,&#10;</xsl:text>
+				</xsl:for-each>
+				<xsl:text>);&#10;</xsl:text>
+				<xsl:for-each select="descendant::rng:element[not(rng:element) and @name != 'data']">
+					<xsl:text>CREATE TABLE </xsl:text>
+					<xsl:value-of select="ancestor::rng:element[parent::*/parent::*/@name='models']/@name"/>
+					<xsl:value-of select="descendant::rng:value"/>
+					<xsl:text> (&#10;</xsl:text>
+					<xsl:text>    </xsl:text>
+					<xsl:value-of select="ancestor::rng:element[parent::*/parent::*/@name='models']/@name"/>
+					<xsl:value-of select="substring('                       ', string-length(ancestor::rng:element[parent::*/parent::*/@name='models']/@name))"/>
+					<xsl:text>uuid,&#10;</xsl:text>					
+					<xsl:text>    </xsl:text>
+					<xsl:value-of select="descendant::rng:value"/>
+					<xsl:value-of select="substring('                       ', string-length(descendant::rng:value))"/>
+					<xsl:text>uuid,&#10;</xsl:text>
+					<xsl:text>    PRIMARY KEY (</xsl:text>
+					<xsl:value-of select="ancestor::rng:element[parent::*/parent::*/@name='models']/@name"/>
+					<xsl:text>, </xsl:text>					
+					<xsl:value-of select="descendant::rng:value"/>
+					<xsl:text>)&#10;</xsl:text>
+					<xsl:text>);&#10;</xsl:text>
+				</xsl:for-each>
+			</xsl:when>
+			<xsl:when test="ancestor::*/@name = 'models' and @name != 'type'">
+				<xsl:text>    </xsl:text>
+				<xsl:value-of select="translate(@name,$uc,$lc)"/>
+				<xsl:value-of select="substring('                       ', string-length(@name))"/>
+				<!-- xsl:value-of select="rng:data/@type"/ -->
+				<xsl:choose>
+					<xsl:when test="rng:data/@type = 'string'">
+						<xsl:text>varchar(40)</xsl:text>
+					</xsl:when>
+					<xsl:when test="rng:data/@type = 'NCName'">
+						<xsl:text>varchar(40) UNIQUE</xsl:text>
+					</xsl:when>
+					<xsl:when test="(rng:data/@type = 'decimal' or rng:data/@type = 'integer') and not (descendant::rng:param)">
+						<xsl:text>integer</xsl:text>
+					</xsl:when>
+					<xsl:when test="(rng:data/@type = 'decimal' or rng:data/@type = 'integer') and descendant::rng:param">
+						<xsl:text>integer</xsl:text>
+						<xsl:text> CHECK (</xsl:text>
+						<xsl:for-each select="descendant::rng:param">
+							<xsl:choose>
+								<xsl:when test="@name = 'minInclusive'">
+									<xsl:value-of select="parent::*/parent::*/@name"/>
+									<xsl:text> &gt;= </xsl:text>
+									<xsl:value-of select="text()"/>
+								</xsl:when>
+								<xsl:when test="@name = 'maxInclusive'">
+									<xsl:value-of select="parent::*/parent::*/@name"/>
+									<xsl:text> &lt;= </xsl:text>
+									<xsl:value-of select="text()"/>
+								</xsl:when>
+							</xsl:choose>
+							<xsl:if test="not(position() = last())">
+								<xsl:text> and </xsl:text>
+							</xsl:if>
+						</xsl:for-each>
+						<xsl:text>)</xsl:text>
+					</xsl:when>
+					<xsl:when test="rng:data/@type = 'positiveInteger'">
+						<xsl:text>integer CHECK (</xsl:text>
+						<xsl:value-of select="@name"/>
+						<xsl:text> &gt; 0)</xsl:text>
+					</xsl:when>
+					<xsl:when test="rng:data/@type = 'nonNegativeInteger'">
+						<xsl:text>integer CHECK (</xsl:text>
+						<xsl:value-of select="@name"/>
+						<xsl:text> &gt;= 0)</xsl:text>
+					</xsl:when>
+					<xsl:when test="rng:data/@type = 'dateTime'">
+						<xsl:text>timestamp</xsl:text>
+					</xsl:when>
+					<xsl:when test="rng:data/@type = 'anyURI'">
+						<xsl:text>varchar(100)</xsl:text>
+					</xsl:when>
+					<xsl:when test="rng:data/@type = 'ID'">
+						<xsl:text>uuid</xsl:text>
+						<xsl:if test="@name = 'id'">
+							<xsl:text> NOT NULL DEFAULT uuid_generate_v4()</xsl:text>
+						</xsl:if>
+					</xsl:when>
+					<xsl:when test="rng:data/@type = 'boolean'">
+						<xsl:text>boolean</xsl:text>
+					</xsl:when>
+					<xsl:when test="child::*[local-name() = 'choice']">
+						<xsl:text>char(10)</xsl:text>
+					</xsl:when>
+				</xsl:choose>
+				<xsl:text>,&#10;</xsl:text>
+			</xsl:when>
+			<xsl:otherwise>
+				<xsl:apply-templates/>
+			</xsl:otherwise>
+		</xsl:choose>
+	</xsl:template>
+	
+	<xsl:template match="text()"/>
+</xsl:stylesheet>
---
+++ modeling/models.sql
@@ -0,0 +1,172 @@
+﻿CREATE TYPE textdirection enum AS ('ltr', 'rtl);
+
+CREATE TABLE project (
+    id                      uuid NOT NULL DEFAULT uuid_generate_v4(),
+    remoteid                uuid,
+    name                    varchar(40),
+    paratextshortname       varchar(40),
+    paratextguid            varchar(40),
+    languagebcp47           varchar(40),
+    languagename            varchar(40),
+    defaultfontfamily       varchar(40),
+    fontfeatures            varchar(40),
+    defaultfontsize         char(10) REFERENCES fontsizevalues(value),
+    textdirection           textdirection,
+    autosync                boolean,
+    allowclaiming           boolean,
+    projecttype             char(10) REFERENCES projecttypevalues(value),
+    destination             char(10) REFERENCES destinationtypevalues(value)
+);
+CREATE TABLE projectset (
+    project                 uuid,
+    set                     uuid,
+    PRIMARY KEY (project, set)
+);
+CREATE TABLE projectuser (
+    project                 uuid,
+    user                    uuid,
+    PRIMARY KEY (project, user)
+);
+CREATE TABLE set (
+    id                      uuid NOT NULL DEFAULT uuid_generate_v4(),
+    remoteid                uuid,
+    book                    integer CHECK (book > 0),
+    index                   integer,
+    settitle                varchar(40)
+);
+CREATE TABLE settask (
+    set                     uuid,
+    task                    uuid,
+    PRIMARY KEY (set, task)
+);
+CREATE TABLE task (
+    id                      uuid NOT NULL DEFAULT uuid_generate_v4(),
+    remoteid                uuid,
+    reference               varchar(40),
+    book                    integer CHECK (book > 0),
+    position                integer,
+    state                   char(10) REFERENCES statevalues(value),
+    hold                    boolean,
+    title                   varchar(40),
+    set                     uuid REFERENCES set (id),
+    assignedto              uuid REFERENCES user (id),
+    lasttranscriber         uuid REFERENCES user (id),
+    lastreviewer            uuid REFERENCES user (id)
+);
+CREATE TABLE taskevent (
+    task                    uuid,
+    event                   uuid,
+    PRIMARY KEY (task, event)
+);
+CREATE TABLE taskmedia (
+    task                    uuid,
+    media                   uuid,
+    PRIMARY KEY (task, media)
+);
+CREATE TABLE media (
+    id                      uuid NOT NULL DEFAULT uuid_generate_v4(),
+    remoteid                uuid,
+    number                  integer CHECK (number > 0),
+    textquality             char(10) REFERENCES textqualityvalues(value),
+    audioquality            char(10) REFERENCES audioqualityvalues(value),
+    duration                integer,
+    transcription           varchar(40),
+    eafurl                  varchar(100),
+    audiourl                varchar(100),
+    task                    uuid REFERENCES task (id)
+);
+CREATE TABLE event (
+    id                      uuid NOT NULL DEFAULT uuid_generate_v4(),
+    remoteid                uuid,
+    historyentry            integer CHECK (historyEntry > 0),
+    datetime                timestamp,
+    action                  char(10) REFERENCES eventactionvalues(value),
+    comment                 varchar(40),
+    set                     uuid REFERENCES set (id),
+    assigneduser            uuid REFERENCES user (id),
+    agent                   uuid REFERENCES user (id)
+);
+CREATE TABLE book (
+    id                      uuid NOT NULL DEFAULT uuid_generate_v4(),
+    remoteid                uuid,
+    shortname               varchar(40),
+    abbr                    varchar(40),
+    longname                varchar(40),
+    index                   integer CHECK (index >= 0),
+    booktype                varchar(10) REFERENCES booktypevalues(value)
+);
+CREATE TABLE bookset (
+    book                    uuid,
+    set                     uuid,
+    PRIMARY KEY (book, set)
+);
+CREATE TABLE user (
+    id                      uuid NOT NULL DEFAULT uuid_generate_v4(),
+    remoteid                uuid,
+    userid                  varchar(40) UNIQUE,
+    fullname                varchar(40),
+    identitytoken           varchar(40),
+    uilanguagebcp47         varchar(40),
+    timer                   char(10) REFERENCES timervalues(value),
+    playbackspeed           integer CHECK (playBackSpeed >= 25 and playBackSpeed <= 200),
+    progressbartype         char(10) REFERENCES progressbarvalues(value),
+    knownlanguages          varchar(40),
+    avatarurl               varchar(100),
+    lasttranscriberproject  uuid REFERENCES project (id),
+    lasttask                uuid REFERENCES task (id),
+    assignedsets            uuid REFERENCES set (id)
+);
+CREATE TABLE userhotkey (
+    user                    uuid,
+	hotkeytype              char(10) REFERENCES hotkeyvalues(value),
+	keycode                 char(10),
+	PRIMARY KEY (user, hotkeytype)
+);
+CREATE TABLE usersetting (
+    user                    uuid,
+    setting                 uuid,
+    PRIMARY KEY (user, setting)
+);
+CREATE TABLE userproject (
+    id                      uuid NOT NULL DEFAULT uuid_generate_v4(),
+    remoteid                uuid,
+    role                    char(10) REFERENCES rolevalues(value),
+    fontfamily              varchar(40),
+    fontfeatures            varchar(40),
+    fontsize                char(10) REFERENCES fontsizevalues(value),
+    user                    uuid REFERENCES user (id),
+    project                 uuid REFERENCES project (id)
+);
+CREATE TABLE rolevalues (
+    value                   varchar(10) PRIMARY KEY
+);
+CREATE TABLE fontsizevalues (
+    value                   varchar(10) PRIMARY KEY
+);
+CREATE TABLE booktypevalues (
+    value                   varchar(10) PRIMARY KEY
+);
+CREATE TABLE projecttypevalues (
+    value                   varchar(10) PRIMARY KEY
+);
+CREATE TABLE destinationvalues (
+    value                   varchar(10) PRIMARY KEY
+);
+CREATE TABLE statevalues (
+    value                   varchar(10) PRIMARY KEY
+);
+CREATE TABLE textqualitvalues (
+    value                   varchar(10) PRIMARY KEY
+);
+CREATE TABLE audioqualitvalues (
+    value                   varchar(10) PRIMARY KEY
+);
+CREATE TABLE eventactionvalues (
+    value                   varchar(10) PRIMARY KEY
+);
+CREATE TABLE timervalues (
+    value                   varchar(10) PRIMARY KEY
+);
+CREATE TABLE hotkeyvalues (
+    value                   varchar(10) PRIMARY KEY
+);
