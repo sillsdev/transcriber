64685010fd99085bfbd0f74c9f19d68e37684003
Greg Trihus <greg_trihus@sil.org>
2019-08-13 15:09:02 -0500

    Test for null or empty name.

--- src/utils/makeAbbr.tsx
+++ src/utils/makeAbbr.tsx
@@ -1,7 +1,9 @@
 export const makeAbbr = (name: string) =>
-  name
-    .trim()
-    .split(' ')
-    .map((s: string) => s.slice(0, 1).toLocaleUpperCase())
-    .join('');
+  name && name.length > 0
+    ? name
+        .trim()
+        .split(' ')
+        .map((s: string) => s.slice(0, 1).toLocaleUpperCase())
+        .join('')
+    : '';
 export default makeAbbr;
