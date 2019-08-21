e5c4528b077bafdb8c2e3c323ef814f7aca8ec5a
gtryus <gtrihus59@yahoo.com>
2019-08-21 17:11:18 -0500

    TT-636 Remove Choose a Plan title (#52)

--- src/components/PlanTable.tsx
+++ src/components/PlanTable.tsx
@@ -7,7 +7,7 @@ import localStrings from '../selector/localize';
 import { withData } from 'react-orbitjs';
 import { QueryBuilder, TransformBuilder } from '@orbit/data';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import { Button, Typography } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
@@ -289,8 +289,6 @@ export function PlanTable(props: IProps) {
               {t.addPlan}
               <AddIcon className={classes.buttonIcon} />
             </Button>
-            <div className={classes.grow} />
-            <Typography variant="h5">{t.choosePlan}</Typography>
             <div className={classes.grow} />
           </div>
           <Grid rows={rows} columns={columns}>
