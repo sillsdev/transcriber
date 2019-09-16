65da0032aa254f18f9cd2aaa91c464ed05242352
Greg Trihus <greg_trihus@sil.org>
2019-09-16 16:36:40 -0500

    No auth callback spinner

--- src/callback/Callback.tsx
+++ src/callback/Callback.tsx
@@ -1,28 +1,8 @@
 import React, { Component } from 'react';
-import loading from './loading.svg';
 
 class Callback extends Component {
   render() {
-    /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-    const style = {
-      position: 'absolute',
-      display: 'flex',
-      justifyContent: 'center',
-      height: '100vh',
-      width: '100vw',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      backgroundColor: 'white',
-    };
-
-    return (
-      // <div style={style}>
-      <div>
-        <img src={loading} alt="loading" />
-      </div>
-    );
+    return <div></div>;
   }
 }
 
