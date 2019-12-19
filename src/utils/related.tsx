e9afbf93b4c2d1461d41a59147e9859d1ef97e23
Sara Hentzel <sara_hentzel@sil.org>
2019-12-19 09:40:17 -0600

    handle when data is undefined (not loaded yet?)

--- src/utils/related.tsx
+++ src/utils/related.tsx
@@ -2,6 +2,8 @@ export const related = (rec: any, key: string) =>
   rec && rec.relationships && key in rec.relationships
     ? rec.relationships[key].data && !Array.isArray(rec.relationships[key].data)
       ? rec.relationships[key].data.id
+      : rec.relationships[key].data == undefined
+      ? null
       : rec.relationships[key].data
     : null;
 export default related;
