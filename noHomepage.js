84f85e38d324c93ed8ea25f844db44b71f772e29
gtryus <gtrihus59@yahoo.com>
2020-02-04 11:33:53 -0600

    Build web w/o homepage (#227)

---
+++ noHomepage.js
@@ -0,0 +1,5 @@
+var fs = require('fs-extra');
+var content = fs.readFileSync('package.json', 'utf-8');
+var json = JSON.parse(content);
+delete json.homepage;
+fs.writeFileSync('package.json', JSON.stringify(json, null, 2) + '\n');
