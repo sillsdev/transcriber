c1cca9c8c92af421bc59ed94ec4ddf50d65ae5fc
gtryus <gtrihus59@yahoo.com>
2020-02-07 16:38:09 -0600

    Enable using electron with dot (#236)

--- edPackage.js
+++ edPackage.js
@@ -1,6 +1,9 @@
 var fs = require('fs-extra');
+
+var argEnv = process.argv.length > 2 ? process.argv[2] : '';
+
 var content = fs.readFileSync('package.json', 'utf-8');
 var json = JSON.parse(content);
-delete json.homepage;
-delete json.main;
+json.main = 'public/electron{0}.js'.replace('{0}', argEnv);
+json.homepage = './';
 fs.writeFileSync('package.json', JSON.stringify(json, null, 2) + '\n');
