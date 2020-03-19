f328032e1758d3aa0bc29dea03cb7e255976be48
Greg Trihus <greg_trihus@sil.org>
2020-03-19 13:56:43 -0500

    change online to check roles

--- src/utils/online.tsx
+++ src/utils/online.tsx
@@ -2,7 +2,7 @@ import Axios from 'axios';
 import { API_CONFIG } from '../api-variable';
 
 export function Online(cb: (result: boolean) => void) {
-  Axios.get(API_CONFIG.host + '/api/projects/', { timeout: 5000 }).catch(
+  Axios.get(API_CONFIG.host + '/api/roles/', { timeout: 5000 }).catch(
     reason => {
       cb(reason.response !== undefined);
     }
