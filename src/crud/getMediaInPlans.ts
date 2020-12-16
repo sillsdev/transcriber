06d1d8ea64e70cc4b141eeacc540cb756cac74a7
sarahentzel <47537281+sarahentzel@users.noreply.github.com>
2020-12-16 14:28:00 -0600

    TT-1870 get latest version based on passage or plan+name if not associated (#678)

--- src/crud/getMediaInPlans.ts
+++ src/crud/getMediaInPlans.ts
@@ -4,6 +4,11 @@ import { related } from '.';
 interface ILatest {
   [planName: string]: number;
 }
+const versionName = (mf: MediaFile) => {
+  const psg = related(mf, 'passage');
+  if (psg) return psg;
+  return related(mf, 'plan') + mf.attributes.originalFile;
+};
 export const getMediaInPlans = (
   planids: Array<string>,
   mediaFiles: Array<MediaFile>
@@ -11,14 +16,12 @@ export const getMediaInPlans = (
   const latest: ILatest = {};
   var media = mediaFiles.filter((m) => planids.includes(related(m, 'plan')));
   media.forEach((f) => {
-    const name = related(f, 'plan') + f.attributes.originalFile;
+    const name = versionName(f);
     latest[name] = latest[name]
       ? Math.max(latest[name], f.attributes.versionNumber)
       : f.attributes.versionNumber;
   });
   return media.filter(
-    (f) =>
-      latest[related(f, 'plan') + f.attributes.originalFile] ===
-      f.attributes.versionNumber
+    (f) => latest[versionName(f)] === f.attributes.versionNumber
   );
 };
