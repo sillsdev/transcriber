bce7a7c7f1df868cc7bbe724de5c7c53bde1be96
Greg Trihus <greg_trihus@sil.org>
2021-02-04 10:32:34 -0600

    TT-1961 make sure to use the correct project type

--- src/crud/useTypeId.ts
+++ src/crud/useTypeId.ts
@@ -3,13 +3,16 @@ import { QueryBuilder, Record } from '@orbit/data';
 
 export const useTypeId = () => {
   const [memory] = useGlobal('memory');
+  const [isOfflineOnly] = useGlobal('offlineOnly');
 
   return (type: string, table: string) => {
     const ptRecs = memory.cache.query((q: QueryBuilder) =>
       q.findRecords(`${table}type`)
     ) as Record[];
     const selected = ptRecs.filter(
-      (p) => p?.attributes?.name?.toLowerCase() === type.toLowerCase()
+      (p) =>
+        p?.attributes?.name?.toLowerCase() === type.toLowerCase() &&
+        Boolean(p?.keys?.remoteId) !== isOfflineOnly
     );
     return selected.length > 0 ? selected[0].id : '';
   };
