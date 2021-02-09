f2e740b4dae0007187589876668f217478ba101d
Greg Trihus <greg_trihus@sil.org>
2021-02-09 10:15:20 -0600

    TT-1991 create folder on upload

--- src/utils/createFolder.ts
+++ src/utils/createFolder.ts
@@ -1,4 +1,5 @@
 import { isElectron } from '../api-variable';
+var path = require('path');
 var fs = isElectron ? require('fs-extra') : undefined;
 
 export const createFolder = (folder: string) => {
@@ -8,4 +9,8 @@ export const createFolder = (folder: string) => {
   } catch (err) {
     if (err.code === 'ENOENT') fs.mkdirSync(folder);
   }
+};
+
+export const createPathFolder = (fullName: string) => {
+  return createFolder(fullName.substring(0, fullName.lastIndexOf(path.sep)));
 };
