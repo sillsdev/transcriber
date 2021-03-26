c497917c16302d70c02834fd6ad2de2823bb31ae
Greg Trihus <greg_trihus@sil.org>
2021-03-26 11:03:44 -0500

    enable desktop media controls

--- src/components/MediaActions.tsx
+++ src/components/MediaActions.tsx
@@ -58,6 +58,7 @@ export function MediaActions(props: IProps) {
     onAttach([rowIndex], !attached);
   };
 
+  console.log(`mediaTab: isElectron=${isElectron}, mediaId=${mediaId}`);
   return (
     <div className={classes.arrangeActions}>
       {!readonly && (
