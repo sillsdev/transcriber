68a621eb420ab456cac8d55af7d5f33a05f84803
gtryus <gtrihus59@yahoo.com>
2021-06-03 08:53:09 -0500

    TT-1471 audacity linux (#236)
    * TT-1471 Audacity Linux Setup
    * Enabling scripts
    * work for web too

--- src/utils/hasAudacityScripts.ts
+++ src/utils/hasAudacityScripts.ts
@@ -1,11 +1,20 @@
 import fs from 'fs';
 import path from 'path';
 import { isElectron } from '../api-variable';
+import { getFiles } from '.';
 const ipc = isElectron ? require('electron').ipcRenderer : null;
+const os = require('os');
 
 const prefsName = async () => {
-  const appData = await ipc?.invoke('appData');
-  return path.join(appData, 'audacity', 'audacity.cfg');
+  if (os.platform() === 'win32') {
+    const appData = await ipc?.invoke('appData');
+    return path.join(appData, 'audacity', 'audacity.cfg');
+  } else {
+    const home = await ipc?.invoke('home');
+    for await (const fullName of getFiles(path.join(home, 'snap'))) {
+      if (fullName.indexOf('audacity.cfg') > 0) return fullName;
+    }
+  }
 };
 
 const getAllPref = async (prefs: string) => {
@@ -13,22 +22,24 @@ const getAllPref = async (prefs: string) => {
   return fs.readFileSync(prefs, 'utf-8');
 };
 
-const getScriptPref = async (content: string) => {
+const getScriptPref = (content: string) => {
   return /mod-script-pipe=([01234])/.exec(content);
 };
 
 export const hasAuacityScripts = async () => {
   const prefs = await prefsName();
+  if (prefs === undefined) return false;
   const content = await getAllPref(prefs);
-  const m = content && (await getScriptPref(content));
+  const m = content && getScriptPref(content);
   return Boolean(m && m[1] === '1');
 };
 
 export const enableAudacityScripts = async () => {
   const prefs = await prefsName();
+  if (prefs === undefined) return false;
   const content = await getAllPref(prefs);
   if (!content) return;
-  const m = await getScriptPref(content);
+  const m = getScriptPref(content);
   if (!m) return;
   const pos = content.indexOf(m[0]) + m[0].length - 1;
   const data = content.slice(0, pos) + '1' + content.slice(pos + 1);
