8c3b4c6a57010938f1e4e87caa161a00549b6668
Greg Trihus <greg_trihus@sil.org>
2021-06-21 14:55:54 -0500

    checkpoint

---
+++ src/components/PlanActionMenu.tsx
@@ -0,0 +1,177 @@
+import React from 'react';
+import { ISharedStrings, IPlanActionsStrings, IState } from '../model';
+import localStrings from '../selector/localize';
+import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import MoreIcon from '@material-ui/icons/MoreHoriz';
+import AssignIcon from '@material-ui/icons/PeopleAltOutlined';
+import DeleteIcon from '@material-ui/icons/DeleteOutline';
+import TranscribeIcon from '@material-ui/icons/EditOutlined';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    paper: {
+      marginRight: theme.spacing(2),
+    },
+  })
+);
+
+interface IStateProps {
+  t: IPlanActionsStrings;
+  ts: ISharedStrings;
+}
+interface IProps extends IStateProps {
+  rowIndex: number;
+  isSection: boolean;
+  isPassage: boolean;
+  mediaId: string;
+  online: boolean;
+  readonly: boolean;
+  isPlaying: boolean;
+  canAssign: boolean;
+  canDelete: boolean;
+  noDeleteNow: boolean;
+  onTranscribe: (i: number) => () => void;
+  onAssign: (where: number[]) => () => void;
+  onDelete: (i: number) => () => void;
+}
+export function PlanActionMenu(props: IProps) {
+  const {
+    t,
+    rowIndex,
+    isSection,
+    isPassage,
+    mediaId,
+    readonly,
+    onTranscribe,
+    onAssign,
+    onDelete,
+    canAssign,
+    canDelete,
+    noDeleteNow,
+  } = props;
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef<HTMLButtonElement>(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: React.MouseEvent<EventTarget>) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event: React.KeyboardEvent) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current!.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  return (
+    <div className={classes.root}>
+      <div>
+        <Button
+          ref={anchorRef}
+          aria-controls={open ? 'menu-list-grow' : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+        >
+          <MoreIcon />
+        </Button>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          placement="right"
+          role={undefined}
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === 'bottom' ? 'center top' : 'center bottom',
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList
+                    autoFocusItem={open}
+                    id="menu-list-grow"
+                    onKeyDown={handleListKeyDown}
+                  >
+                    {isSection && canAssign && !readonly && (
+                      <MenuItem
+                        id="planActAssign"
+                        title={t.assign}
+                        onClick={onAssign([rowIndex])}
+                      >
+                        <AssignIcon />
+                      </MenuItem>
+                    )}
+                    {isPassage && (
+                      <MenuItem
+                        id="planActTrans"
+                        title={t.transcribe}
+                        onClick={onTranscribe(rowIndex)}
+                        disabled={(mediaId || '') === ''}
+                      >
+                        <TranscribeIcon />
+                      </MenuItem>
+                    )}
+                    {canDelete && !readonly && (
+                      <MenuItem
+                        id="planActDel"
+                        title={t.delete}
+                        onClick={onDelete(rowIndex)}
+                        disabled={noDeleteNow}
+                      >
+                        <DeleteIcon />
+                      </MenuItem>
+                    )}
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = (state: IState): IStateProps => ({
+  t: localStrings(state, { layout: 'planActions' }),
+  ts: localStrings(state, { layout: 'shared' }),
+});
+export default connect(mapStateToProps)(PlanActionMenu) as any as any;
