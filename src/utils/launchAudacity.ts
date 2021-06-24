e29c1714d33bbe6322d45f94bc0e73b6835632b9
Greg Trihus <greg_trihus@sil.org>
2021-06-24 09:50:52 -0500

    get launch working

--- src/utils/launchAudacity.ts
+++ src/utils/launchAudacity.ts
@@ -1,14 +1,22 @@
 import { IExeca } from '../model';
-import { dataPath, PathType } from '.';
+import { dataPath, PathType, getRegVal } from '.';
 import { API_CONFIG } from '../api-variable';
 const isElectron = process.env.REACT_APP_MODE === 'electron';
 const execa = isElectron ? require('execa') : null;
 
 export const launchAudacity = async (mediaName: string) => {
+  const key = 'HKCR\\Python.CompiledFile\\shell\\open\\command';
+  const python = await getRegVal(key, '');
+  const pythonExe = python?.split(' ')[0].split('"')[0];
   const curPath = dataPath(mediaName, PathType.MEDIA);
-  const cmd = `python.exe audacity-pipe.py "${curPath}"`;
-  console.log(`launching "${cmd}"`, API_CONFIG);
-  execa(cmd, { cwd: API_CONFIG.resourcePath, detached: true })
+  // const cmd = `${pythonExe} audacity-pipe.py "${curPath}"`;
+  // console.log(`launching "${cmd}"`, API_CONFIG);
+  execa(pythonExe, ['audacity-pipe.py', `"${curPath}"`], {
+    shell: true,
+    detached: true,
+    cwd: API_CONFIG.resourcePath,
+    env: { ...{ ...process }.env, DISPLAY: ':0' },
+  })
     .then((res: IExeca) => {
       console.log(res);
       if (typeof res.stdout === 'string') {
