0fb28ab49c0959f28a92895a680b54d94a58925a
Greg Trihus <greg_trihus@sil.org>
2021-07-23 10:23:51 -0500

    strings Guid

--- src/store/localization/actions.tsx
+++ src/store/localization/actions.tsx
@@ -1,9 +1,10 @@
 import Axios from 'axios';
 import { FETCH_LOCALIZATION, SET_LANGUAGE } from './types';
 import { appPath } from '../../utils';
+import { stringsName } from './exported-strings-name.json';
 
 export const fetchLocalization = () => (dispatch: any) => {
-  Axios.get(appPath() + '/localization/strings.json').then(strings => {
+  Axios.get(appPath() + '/localization/' + stringsName).then((strings) => {
     dispatch({
       payload: strings,
       type: FETCH_LOCALIZATION,
