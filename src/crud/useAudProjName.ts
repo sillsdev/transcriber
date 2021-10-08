2a9f177192c17510936b03e1e8b0f5000da821bf
gtryus <gtrihus59@yahoo.com>
2021-10-08 17:08:52 -0500

    TT-2939 Unique Audacity Names (#453)

--- src/crud/useAudProjName.ts
+++ src/crud/useAudProjName.ts
@@ -1,19 +1,25 @@
 import { useGlobal } from 'reactn';
 import { Plan, Passage, Section } from '../model';
-import { RecordIdentity } from '@orbit/data';
+import { RecordIdentity, Record } from '@orbit/data';
 import { related, usePlan } from '.';
 import { toCamel, pad2, cleanFileName } from '../utils';
 import { isElectron } from '../api-variable';
 const ipc = isElectron ? require('electron').ipcRenderer : null;
 const path = require('path');
 
-const makeSlug = (rec: Plan | null) => {
+const planSlug = (rec: Plan | null) => {
   const name = rec?.attributes?.name || '';
   return (
     rec?.attributes?.slug ||
     toCamel(cleanFileName(name.replace(' ', '_'))).slice(0, 6) +
       rec?.id.slice(0, 4)
   );
+};
+
+const recSlug = (rec: Record, seq: number) => {
+  return `-${pad2(seq)}${rec.type.slice(0, 1)}${
+    rec?.keys?.remoteId || rec?.id.slice(0, 4)
+  }`;
 };
 
 export const useAudProjName = () => {
@@ -31,21 +37,20 @@ export const useAudProjName = () => {
     const planRec = getPlan(related(secRec, 'plan'));
     const docs = await ipc?.invoke('getPath', 'documents');
     const book = passRec?.attributes?.book;
-    const secseq = secRec.attributes.sequencenum;
-    let secPart = `${book ? `${book}-` : ''}s${pad2(secseq)}`;
+    const secSeq = secRec?.attributes?.sequencenum || 0;
+    let secPart = `${book ? '-' + book : ''}${recSlug(secRec, secSeq)}`;
     const ref = passRec?.attributes?.reference;
-    const cleanRef = cleanFileName(ref ? ref.replace(' ', '_') : '');
-    let aupPath = path.join(docs, 'Audacity', makeSlug(planRec));
+    const cleanRef = ref ? `-${cleanFileName(ref.replace(' ', '_'))}` : '';
+    let aupPath = path.join(docs, 'Audacity', planSlug(planRec));
     let pasPart = '';
     if (planRec?.attributes?.flat) {
-      secPart += `-${cleanRef}`;
-      aupPath = path.join(aupPath, secPart);
+      secPart += cleanRef;
+      aupPath = path.join(aupPath, secPart.slice(1));
     } else {
-      const pasSeq = passRec?.attributes?.sequencenum;
-      pasPart = `p${pad2(pasSeq)}-${cleanRef}`;
-      aupPath = path.join(aupPath, secPart, pasPart);
-      pasPart = `-${pasPart}`;
+      const pasSeq = passRec?.attributes?.sequencenum || 0;
+      pasPart = cleanRef + recSlug(passRec, pasSeq);
+      aupPath = path.join(aupPath, secPart.slice(1), pasPart.slice(1));
     }
-    return path.join(aupPath, `${makeSlug(planRec)}-${secPart}${pasPart}.aup3`);
+    return path.join(aupPath, `${planSlug(planRec)}${secPart}${pasPart}.aup3`);
   };
 };
