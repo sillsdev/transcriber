513175157504ab717178ed8d15291367610c3ecd
Greg Trihus <greg_trihus@sil.org>
2021-10-08 13:07:02 -0500

    TT-2828d Audacity errors
    Report errors in Audacity linkage

--- src/utils/launchAudacity.ts
+++ src/utils/launchAudacity.ts
@@ -1,10 +1,21 @@
 import { IExeca } from '../model';
-import { dataPath, PathType, getPythonExe } from '.';
+import {
+  dataPath,
+  PathType,
+  getPythonExe,
+  logError,
+  Severity,
+  infoMsg,
+} from '.';
 import { API_CONFIG } from '../api-variable';
 const isElectron = process.env.REACT_APP_MODE === 'electron';
 const execa = isElectron ? require('execa') : null;
 
-export const launchAudacity = async (proj: string, mediaName?: string) => {
+export const launchAudacity = async (
+  proj: string,
+  reporter: any,
+  mediaName?: string
+) => {
   const pythonExe = await getPythonExe();
   const args = ['audacity-pipe.py', `"${proj}"`];
   if (mediaName && mediaName !== '') {
@@ -18,16 +29,18 @@ export const launchAudacity = async (proj: string, mediaName?: string) => {
     env: { ...{ ...process }.env },
   })
     .then((res: IExeca) => {
-      if (typeof res.stdout === 'string') {
-        res.stdout.split('\n').forEach((ln: string) => {
-          console.log(ln);
-        });
+      if (typeof res?.stdout === 'string' && res.stdout.trim().length > 0) {
+        const msg = `Launch Audacity Results:\n${res.stdout}`;
+        logError(Severity.info, reporter, msg);
       }
     })
     .catch((err: any) => {
-      console.error(JSON.stringify(err, null, 2));
-      err.stdout.split('\n').forEach((ln: string) => {
-        console.log(ln);
-      });
+      const launchErr = 'Launch Audacity Error';
+      if (typeof err?.stdout === 'string' && err.stdout.trim().length > 0) {
+        let msg = `${launchErr}\n${err.stdout}`;
+        logError(Severity.error, reporter, infoMsg(err, msg));
+      } else {
+        logError(Severity.error, reporter, infoMsg(err, launchErr));
+      }
     });
 };
