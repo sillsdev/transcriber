3d5c502ec1c2fe6537dde112aeb17dbc6d0f4e78
Greg Trihus <greg_trihus@sil.org>
2021-11-23 13:29:30 -0600

    TT-3016 delete resource media with resource

--- src/crud/useSecResDelete.ts
+++ src/crud/useSecResDelete.ts
@@ -1,11 +1,15 @@
 import { useGlobal } from 'reactn';
 import { SectionResource } from '../model';
 import { TransformBuilder } from '@orbit/data';
+import { related } from '.';
 
 export const useSecResDelete = () => {
   const [memory] = useGlobal('memory');
 
-  return (secResRec: SectionResource) => {
-    memory.update((t: TransformBuilder) => t.removeRecord(secResRec));
+  return async (secResRec: SectionResource) => {
+    const mediaId = related(secResRec, 'mediafile');
+    const mediaRecId = { type: 'mediafile', id: mediaId };
+    await memory.update((t: TransformBuilder) => t.removeRecord(secResRec));
+    await memory.update((t: TransformBuilder) => t.removeRecord(mediaRecId));
   };
 };
