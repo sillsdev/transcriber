84f9221782f3ebc02191dcab3cdaf55a5cf072e4
Greg Trihus <greg_trihus@sil.org>
2022-02-07 14:52:51 -0600

    Add tests for step lookup

--- src/__tests__/getWorkflow.test.ts
+++ src/__tests__/getWorkflow.test.ts
@@ -1,10 +1,22 @@
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { Section, Passage, IWorkflow, IWorkflowStepsStrings } from '../model';
+import {
+  Section,
+  Passage,
+  IWorkflow,
+  OrgWorkflowStep,
+  IWorkflowStepsStrings,
+} from '../model';
 import { getWorkflow } from '../components/Workflow';
 import { memory } from '../schema';
 
-const wfStr = {} as IWorkflowStepsStrings;
+const wfStr = {
+  internalization: 'Internalization',
+  peerReview: 'Peer Review',
+  record: 'Record',
+  teamCheck: 'Team Check',
+  getString: (v: string) => (wfStr as any)[v],
+} as IWorkflowStepsStrings;
 
 const s1: Section = {
   type: 'section',
@@ -180,6 +192,85 @@ const pa11: Passage = {
     lastModifiedByUser: { data: { type: 'user', id: 'u0' } },
   },
 } as Passage;
+
+const owf: OrgWorkflowStep[] = [
+  {
+    type: 'orgworkflowstep',
+    id: 'owf1',
+    keys: { remoteId: '1' },
+    attributes: {
+      process: 'OBT',
+      name: 'Internalization',
+      sequencenum: 1,
+      tool: '{"tool": "resource"}',
+      permissions: '{}',
+      dateCreated: '2021-09-14',
+      dateUpdated: '2021-09-15',
+      lastModifiedBy: 1,
+    },
+    relationships: {
+      organization: { data: { type: 'organization', id: 'o0' } },
+      lastModifiedByUser: { data: { type: 'user', id: 'u0' } },
+    },
+  },
+  {
+    type: 'orgworkflowstep',
+    id: 'owf2',
+    keys: { remoteId: '2' },
+    attributes: {
+      process: 'OBT',
+      name: 'Record',
+      sequencenum: 2,
+      tool: '{"tool": "record"}',
+      permissions: '{}',
+      dateCreated: '2021-09-14',
+      dateUpdated: '2021-09-15',
+      lastModifiedBy: 1,
+    },
+    relationships: {
+      organization: { data: { type: 'organization', id: 'o0' } },
+      lastModifiedByUser: { data: { type: 'user', id: 'u0' } },
+    },
+  },
+  {
+    type: 'orgworkflowstep',
+    id: 'owf3',
+    keys: { remoteId: '3' },
+    attributes: {
+      process: 'OBT',
+      name: 'TeamCheck',
+      sequencenum: 3,
+      tool: '{"tool": "teamCheck"}',
+      permissions: '{}',
+      dateCreated: '2021-09-14',
+      dateUpdated: '2021-09-15',
+      lastModifiedBy: 1,
+    },
+    relationships: {
+      organization: { data: { type: 'organization', id: 'o0' } },
+      lastModifiedByUser: { data: { type: 'user', id: 'u0' } },
+    },
+  },
+  {
+    type: 'orgworkflowstep',
+    id: 'owf4',
+    keys: { remoteId: '4' },
+    attributes: {
+      process: 'OBT',
+      name: 'PeerReview',
+      sequencenum: 4,
+      tool: '{"tool": "teamCheck"}',
+      permissions: '{}',
+      dateCreated: '2022-01-28T16:43:40.929',
+      dateUpdated: '2022-01-28T16:43:40.929',
+      lastModifiedBy: 1,
+    },
+    relationships: {
+      organization: { data: { type: 'organization', id: 'o0' } },
+      lastModifiedByUser: { data: { type: 'user', id: 'u0' } },
+    },
+  },
+];
 
 afterEach(cleanup);
 
@@ -677,6 +768,105 @@ test('update one flat section to two flat section ignoring other plan', async ()
     {
       level: 0,
       kind: 2,
+      sectionSeq: 2,
+      title: 'Birth of John',
+      passageSeq: 1,
+      book: 'LUK',
+      reference: '1:11-14',
+      comment: 'Zechariah at the temple',
+      sectionId: { type: 'section', id: 's2' },
+      sectionUpdated: '2021-09-15',
+      passageId: { type: 'passage', id: 'pa4' },
+      passageUpdated: '2021-09-15',
+      transcriber: undefined,
+      editor: undefined,
+      mediaId: undefined,
+      mediaShared: 3,
+      deleted: false,
+    },
+  ] as IWorkflow[]);
+});
+
+test('one section and one passage with step gives output', async () => {
+  expect(
+    getWorkflow('pl1', [s1], [pa1], false, false, memory, 'fakeVId', owf, wfStr)
+  ).toEqual([
+    {
+      level: 0,
+      kind: 0,
+      sectionSeq: 1,
+      title: 'Intro',
+      passageSeq: 0,
+      sectionId: { type: 'section', id: 's1' },
+      sectionUpdated: '2021-09-15',
+      transcriber: undefined,
+      editor: undefined,
+      deleted: false,
+    },
+    {
+      level: 1,
+      kind: 1,
+      step: 'Internalization',
+      sectionSeq: 1,
+      passageSeq: 1,
+      book: 'LUK',
+      reference: '1:1-4',
+      comment: 'salutation',
+      passageUpdated: '2021-09-15',
+      passageId: { type: 'passage', id: 'pa1' },
+      mediaId: undefined,
+      mediaShared: 3,
+      deleted: false,
+    },
+  ] as IWorkflow[]);
+});
+
+test('two flat sections with steps gives output', async () => {
+  const pa1b = {
+    ...pa1,
+    attributes: {
+      ...pa1.attributes,
+      stepComplete:
+        '{"completed": [{"name": "Internalization", "stepid": "1", "complete": true}, {"name": "Record", "stepid": "2", "complete": true}, {"name": "TeamCheck", "stepid": "3", "complete": true}]}',
+    },
+  } as Passage;
+  expect(
+    getWorkflow(
+      'pl1',
+      [s1, s2],
+      [pa1b, pa4, pa11],
+      true,
+      false,
+      memory,
+      'fakeVId',
+      owf,
+      wfStr
+    )
+  ).toEqual([
+    {
+      level: 0,
+      kind: 2,
+      step: 'Peer Review',
+      sectionSeq: 1,
+      title: 'Intro',
+      passageSeq: 1,
+      book: 'LUK',
+      reference: '1:1-4',
+      comment: 'salutation',
+      sectionId: { type: 'section', id: 's1' },
+      sectionUpdated: '2021-09-15',
+      passageId: { type: 'passage', id: 'pa1' },
+      passageUpdated: '2021-09-15',
+      transcriber: undefined,
+      editor: undefined,
+      mediaId: undefined,
+      mediaShared: 3,
+      deleted: false,
+    },
+    {
+      level: 0,
+      kind: 2,
+      step: 'Internalization',
       sectionSeq: 2,
       title: 'Birth of John',
       passageSeq: 1,
