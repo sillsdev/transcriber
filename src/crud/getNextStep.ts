489f2d9581de975a45a661de3d96dce8c6256ddb
Greg Trihus <greg_trihus@sil.org>
2022-02-09 11:19:18 -0600

    TT-3362 finding next step

--- src/crud/getNextStep.ts
+++ src/crud/getNextStep.ts
@@ -7,10 +7,10 @@ interface IGetNextStepProps {
 
 export const getNextStep = (state: IGetNextStepProps) => {
   let nextIndex = 0;
-  let completeLen = state.psgCompleted.length;
   for (let w of state.orgWorkflowSteps) {
-    const pcItem =
-      nextIndex < completeLen ? state.psgCompleted[nextIndex] : undefined;
+    const pcItem = state.psgCompleted.find(
+      (s) => s.name === w.attributes?.name
+    );
     const id = w.keys?.remoteId || w.id;
     if (id === pcItem?.stepid && pcItem?.complete) {
       nextIndex += 1;
