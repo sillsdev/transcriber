8f39ec6c3913774c0a55727ef47898c92ca3e1cd
gtryus <greg_trihus@sil.org>
2022-02-23 22:27:04 -0600

    Passage Item transcribe tooltip (#707)

--- src/store/localization/reducers.tsx
+++ src/store/localization/reducers.tsx
@@ -196,6 +196,7 @@ const initialState = {
 			"saveFirst": "Save or Clear your recording before uploading.",
 			"segment": "Segment",
 			"speaker": "Speaker",
+			"transcribe": "Transcribe",
 			"unsaved": "Unsaved",
 			"uploadComplete": "{0} of {1} files uploaded successfully.",
 		}
