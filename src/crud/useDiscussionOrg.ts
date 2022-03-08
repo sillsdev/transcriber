3557bd2f38df29722eb7fa7dedf32551b4ae3721
sarahentzel <47537281+sarahentzel@users.noreply.github.com>
2022-03-08 16:44:40 -0600

    TT-3771 clear import directory of old files before new import (#719)
    don't use q.findRecord if it might not be there

--- src/crud/useDiscussionOrg.ts
+++ src/crud/useDiscussionOrg.ts
@@ -1,17 +1,13 @@
 import { useGlobal } from 'reactn';
 import { Discussion } from '../model';
-import { QueryBuilder } from '@orbit/data';
-import { related } from '.';
+import { findRecord, related } from '.';
 
 export const useDiscussionOrg = () => {
   const [memory] = useGlobal('memory');
   return (d: Discussion) => {
     const id = related(d, 'orgWorkflowStep');
     if (!id) return '';
-    const stepRecId = { type: 'orgworkflowstep', id };
-    const rec = memory.cache.query((q: QueryBuilder) =>
-      q.findRecord(stepRecId)
-    );
+    const rec = findRecord(memory, 'orgworkflowstep', id);
     return related(rec, 'organization');
   };
 };
