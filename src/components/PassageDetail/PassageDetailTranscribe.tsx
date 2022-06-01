439c68830b1db91dd5c4fbb01bf4c09f8c21803c
Greg Trihus <greg_trihus@sil.org>
2022-06-01 14:40:35 -0500

    TT-3998 Add Transcriber to Workflow

--- src/components/PassageDetail/PassageDetailTranscribe.tsx
+++ src/components/PassageDetail/PassageDetailTranscribe.tsx
@@ -1,56 +1,21 @@
-import { useState } from 'react';
-import { connect } from 'react-redux';
-import { IState, ITranscribeStrings } from '../../model';
-import { useLocation } from 'react-router-dom';
-import localStrings from '../../selector/localize';
-import { Button, makeStyles, Theme, createStyles } from '@material-ui/core';
-import TranscribeIcon from '../../control/TranscribeIcon';
-import { LocalKey, localUserKey } from '../../utils';
-import StickyRedirect from '../StickyRedirect';
+import { Grid } from '@material-ui/core';
+import { TranscriberProvider } from '../../context/TranscriberContext';
+import Transcriber from '../../components/Transcriber';
+import Auth from '../../auth/Auth';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    icon: {
-      display: 'flex',
-      paddingLeft: theme.spacing(1),
-    },
-  })
-);
-
-interface IStateProps {
-  t: ITranscribeStrings;
+interface IProps {
+  auth: Auth;
+  width: number;
 }
 
-interface IProps extends IStateProps {}
-
-export function PassageDetailTranscribe(props: IProps) {
-  const { t } = props;
-  const { pathname } = useLocation();
-  const classes = useStyles();
-  const [view, setView] = useState('');
-
-  const handleTranscribe = () => {
-    localStorage.setItem(localUserKey(LocalKey.jumpBack), pathname);
-    setView(pathname.replace('detail', 'work'));
-  };
-
-  if (view) return <StickyRedirect to={view} />;
-
+export function PassageDetailTranscribe({ auth, width }: IProps) {
   return (
-    <Button
-      id="transcriber-step"
-      onClick={handleTranscribe}
-      variant="contained"
-      color="primary"
-    >
-      {t.openTranscriber}
-      <TranscribeIcon className={classes.icon} color="white" />
-    </Button>
+    <TranscriberProvider auth={auth}>
+      <Grid container direction="column">
+        <Transcriber auth={auth} defaultWidth={width}/>
+      </Grid>
+    </TranscriberProvider>
   );
 }
 
-const mapStateToProps = (state: IState): IStateProps => ({
-  t: localStrings(state, { layout: 'transcribe' }),
-});
-
-export default connect(mapStateToProps)(PassageDetailTranscribe) as any as any;
+export default PassageDetailTranscribe as any;
