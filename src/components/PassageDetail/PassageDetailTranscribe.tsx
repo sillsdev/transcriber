53d5e4b546bc1f0f08b94f47cb093a51275cd6c1
Greg Trihus <greg_trihus@sil.org>
2022-06-03 14:25:38 -0500

    TT-3998d Transcriber w/o audio

--- src/components/PassageDetail/PassageDetailTranscribe.tsx
+++ src/components/PassageDetail/PassageDetailTranscribe.tsx
@@ -2,6 +2,7 @@ import { Grid } from '@material-ui/core';
 import { TranscriberProvider } from '../../context/TranscriberContext';
 import Transcriber from '../../components/Transcriber';
 import Auth from '../../auth/Auth';
+import usePassageDetailContext from '../../context/usePassageDetailContext';
 
 interface IProps {
   auth: Auth;
@@ -9,12 +10,15 @@ interface IProps {
 }
 
 export function PassageDetailTranscribe({ auth, width }: IProps) {
-  return (
+  const { mediafileId } = usePassageDetailContext();
+  return Boolean(mediafileId) ? (
     <TranscriberProvider auth={auth}>
       <Grid container direction="column">
-        <Transcriber auth={auth} defaultWidth={width}/>
+        <Transcriber auth={auth} defaultWidth={width} />
       </Grid>
     </TranscriberProvider>
+  ) : (
+    <></>
   );
 }
 
