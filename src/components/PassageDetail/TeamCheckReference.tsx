3f86602a5b13c4e024fc718d7e9f4226d4701aab
Greg Trihus <greg_trihus@sil.org>
2022-06-23 16:29:51 -0500

    TT-4020 Comparing Project Resource

--- src/components/PassageDetail/TeamCheckReference.tsx
+++ src/components/PassageDetail/TeamCheckReference.tsx
@@ -1,9 +1,10 @@
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import { Grid, makeStyles, createStyles, Theme } from '@material-ui/core';
 import SelectMyResource from './Internalization/SelectMyResource';
 import { MediaPlayer } from '../MediaPlayer';
 import Auth from '../../auth/Auth';
 import { PassageDetailContext } from '../../context/PassageDetailContext';
+import { getSegments, NamedRegions } from '../../utils';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -21,7 +22,6 @@ const useStyles = makeStyles((theme: Theme) =>
         width: 'inherit',
       },
     },
-    slider: {},
     controls: {
       alignSelf: 'center',
     },
@@ -36,15 +36,60 @@ export function TeamCheckReference({ auth }: IProps) {
   const classes = useStyles();
   const ctx = useContext(PassageDetailContext);
   const {
+    rowData,
     playItem,
     setPlayItem,
+    setMediaSelected,
     itemPlaying,
+    setItemPlaying,
     handleItemPlayEnd,
     handleItemTogglePlay,
   } = ctx.state;
+  const mediaStart = useRef<number | undefined>();
+  const mediaEnd = useRef<number | undefined>();
+  const mediaPosition = useRef<number | undefined>();
 
   const handleResource = (id: string) => {
+    const row = rowData.find((r) => r.id === id);
+    if (row) {
+      const segs = getSegments(
+        NamedRegions.ProjectResource,
+        row.mediafile.attributes.segments
+      );
+      const regions = JSON.parse(segs);
+      if (regions.length > 0) {
+        const { start, end } = regions[0];
+        mediaStart.current = start;
+        mediaEnd.current = end;
+        setMediaSelected(id, start, end);
+        return;
+      }
+    }
     setPlayItem(id);
+  };
+
+  const handleEnded = () => {
+    mediaStart.current = undefined;
+    mediaEnd.current = undefined;
+    mediaPosition.current = undefined;
+    setPlayItem('');
+    handleItemPlayEnd();
+  };
+
+  const handleDuration = (duration: number) => {
+    if (mediaStart.current) {
+      mediaPosition.current = mediaStart.current;
+      mediaStart.current = undefined;
+      setItemPlaying(true);
+    }
+  };
+
+  const handlePosition = (position: number) => {
+    if (mediaEnd.current) {
+      if (position >= mediaEnd.current) {
+        handleEnded();
+      }
+    }
   };
 
   return (
@@ -58,7 +103,10 @@ export function TeamCheckReference({ auth }: IProps) {
           srcMediaId={playItem}
           requestPlay={itemPlaying}
           onTogglePlay={handleItemTogglePlay}
-          onEnded={handleItemPlayEnd}
+          onEnded={handleEnded}
+          onDuration={handleDuration}
+          onPosition={handlePosition}
+          position={mediaPosition.current}
           controls={true}
         />
       </Grid>
