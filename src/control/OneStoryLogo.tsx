71ac04a6019e86afb4092820f4d5a6bc41a26d75
Greg Trihus <greg_trihus@sil.org>
2022-08-17 19:48:14 -0500

    TT-4163 Logos

--- src/control/OneStoryLogo.tsx
+++ src/control/OneStoryLogo.tsx
@@ -1,19 +1,7 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import oneStoryLogo from '../assets/oneStory.png';
-
-const useStyles = makeStyles({
-  logo: {
-    width: '16px',
-    height: '16px',
-  },
-});
+import { LogoImg } from '.';
 
 export const OneStoryLogo = () => {
-  const classes = useStyles();
-
-  return (
-    <img src={oneStoryLogo} alt="OneStory Logo" className={classes.logo} />
-  );
+  return <LogoImg src={oneStoryLogo} alt="OneStory Logo" />;
 };
 export default OneStoryLogo;
--- src/control/ParatextLogo.tsx
+++ src/control/ParatextLogo.tsx
@@ -1,19 +1,7 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import paratextLogo from '../assets/p9logo312o-150x150.png';
-
-const useStyles = makeStyles({
-  logo: {
-    width: '16px',
-    height: '16px',
-  },
-});
+import { LogoImg } from '.';
 
 export const ParatextLogo = () => {
-  const classes = useStyles();
-
-  return (
-    <img src={paratextLogo} alt="Paratext Logo" className={classes.logo} />
-  );
+  return <LogoImg src={paratextLogo} alt="Paratext Logo" />;
 };
 export default ParatextLogo;
