fb90406cd47ee50ee750196d5999f98883fc7bf9
Greg Trihus <greg_trihus@sil.org>
2022-08-19 12:45:15 -0500

    TT-4163 AllDone

--- src/components/AllDone.tsx
+++ src/components/AllDone.tsx
@@ -1,33 +1,24 @@
 import React from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core';
-import { Typography } from '@material-ui/core';
+import { Typography, Box, styled } from '@mui/material';
 import success from '../assets/success.png';
 import { TranscriberContext } from '../context/TranscriberContext';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignContent: 'center',
-    },
-    image: {
-      width: '343px',
-      alignSelf: 'center',
-      padding: theme.spacing(3),
-    },
-  })
-);
+const DoneImg = styled('img')(({ theme }) => ({
+  width: '343px',
+  alignSelf: 'center',
+  padding: theme.spacing(3),
+}));
 
 export const AllDone = () => {
-  const classes = useStyles();
   const ctx = React.useContext(TranscriberContext);
   const { transcriberStr } = ctx.state;
   const t = transcriberStr;
 
   return (
-    <div className={classes.root}>
-      <img src={success} alt="Success!" className={classes.image} />
+    <Box
+      sx={{ display: 'flex', flexDirection: 'column', alignContent: 'center' }}
+    >
+      <DoneImg src={success} alt="Success!" />
       <Typography variant="h1" align="center">
         {t.congratulation}
       </Typography>
@@ -36,6 +27,6 @@ export const AllDone = () => {
         {t.noMoreTasks}
       </Typography>
       {'\u00A0'}
-    </div>
+    </Box>
   );
 };
