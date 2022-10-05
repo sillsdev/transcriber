398819cdc061185b6ad89730722ddd636aa992b3
Greg Trihus <greg_trihus@sil.org>
2022-10-04 20:06:50 -0500

    TT-4685 Update logo on About page

--- src/control/ApmLogo.tsx
+++ src/control/ApmLogo.tsx
@@ -1,11 +1,16 @@
 import apmLogo from '../assets/apm-logo.min.svg';
 import { LogoImg } from '.';
+import { SxProps } from '@mui/material';
 
-export const ApmLogo = () => {
+interface IProps {
+  sx?: SxProps;
+}
+
+export const ApmLogo = (props: IProps) => {
   return (
     <LogoImg
       src={apmLogo}
-      sx={{ alignSelf: 'center', width: '256px', height: '256px' }}
+      sx={{ alignSelf: 'center', width: '256px', height: '256px', ...props.sx }}
       alt="Audio Project Manager Logo"
     />
   );
