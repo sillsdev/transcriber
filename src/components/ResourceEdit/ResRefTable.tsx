43e9354a0d7284a583e8035f517fc305277cd66e
Greg Trihus <greg_trihus@sil.org>
2023-03-24 13:43:45 -0500

    Check for bad refs or empty book
    - confirm saving data if it has errors ignoring the errors

--- src/components/ResourceEdit/ResRefTable.tsx
+++ src/components/ResourceEdit/ResRefTable.tsx
@@ -14,6 +14,15 @@ const t = {
   action: 'Action',
   bookSelect: 'Select Book',
   deleteConfirm: 'Delete {0}',
+  errorData: 'Do you want to save and discard data with errors?',
+};
+
+const refPat =
+  /((\d+)(?::(\d+))?(\s*-\s*(\d+))?\s*;\s*)*(\d+)(?::(\d+))?(\s*-\s*(\d+))?/;
+
+const refTest = (s: string) => {
+  const m = refPat.exec(s);
+  return m && m[0].length === s.length;
 };
 
 const Content = styled(Box)<BoxProps>(({ theme }) => ({
@@ -22,6 +31,9 @@ const Content = styled(Box)<BoxProps>(({ theme }) => ({
     textAlign: 'left',
     paddingLeft: theme.spacing(0.5),
     paddingRight: theme.spacing(0.5),
+  },
+  '& .data-grid-container .data-grid .Err': {
+    backgroundColor: 'orange',
   },
 }));
 
@@ -57,6 +69,7 @@ export default function ReferenceTable({
   const [data, setData] = React.useState<ICell[][]>([]);
   const dataRef = React.useRef<ICell[][]>([]);
   const [confirmRow, setConfirmRow] = React.useState<number>();
+  const [confirmErr, setConfirmErr] = React.useState<BookRef[]>();
   const bookMap = useSelector((state: IState) => state.books.map);
   const bookSuggestions = useSelector(
     (state: IState) => state.books.suggestions
@@ -136,7 +149,10 @@ export default function ReferenceTable({
             value: v,
             width: widths[i],
             readOnly: n === 0 || readOnlys[i],
-            className: n === 0 ? 'cTitle' : cClass[i],
+            className:
+              n === 0
+                ? 'cTitle'
+                : cClass[i] + (!v && row[ColName.Refs] ? ' Err' : ''),
             dataEditor: bookEditor,
           } as ICell)
         : i === ColName.Act && n > 0
@@ -151,7 +167,11 @@ export default function ReferenceTable({
             value: v,
             width: widths[i],
             readOnly: n === 0 || readOnlys[i],
-            className: n === 0 ? 'cTitle' : cClass[i],
+            className:
+              n === 0
+                ? 'cTitle'
+                : cClass[i] +
+                  (i === ColName.Refs && v && !refTest(v) ? ' Err' : ''),
           } as ICell)
     );
 
@@ -173,15 +193,33 @@ export default function ReferenceTable({
   const handleSave = () => {
     if (preventSave.current) return;
     const refData: BookRef[] = [];
+    let error = 0;
+    const last = dataRef.current.length - 1;
     dataRef.current.forEach((r, i) => {
-      if (i > 0 && r[ColName.Book].value) {
+      if (
+        (i < last && !r[ColName.Book].value) ||
+        (i > 0 && r[ColName.Refs].value && !refTest(r[ColName.Refs].value))
+      ) {
+        error++;
+      } else if (i > 0 && r[ColName.Book].value) {
         refData.push({
           code: r[ColName.Book].value,
           refs: r[ColName.Refs].value,
         });
       }
     });
-    onCommit(refData);
+    if (error > 0) {
+      setConfirmErr(refData);
+    } else {
+      onCommit(refData);
+    }
+  };
+  const saveConfirm = () => {
+    if (confirmErr) onCommit(confirmErr);
+    setConfirmErr(undefined);
+  };
+  const saveRejected = () => {
+    setConfirmErr(undefined);
   };
 
   const handleValueRenderer = (cell: ICell) =>
@@ -192,7 +230,16 @@ export default function ReferenceTable({
   const handleCellsChanged = (changes: Array<ICellChange>) => {
     const newData = dataRef.current.map((r) => r);
     changes.forEach((c) => {
+      const bookCng =
+        c.col === ColName.Book &&
+        Boolean(c.value) !== Boolean(newData[c.row][c.col].value);
       newData[c.row][c.col].value = c.value;
+      if (bookCng || c.col === ColName.Refs)
+        // Turns error background on and off
+        newData[c.row] = rowCells(
+          c.row,
+          newData[c.row].map((i) => i.value)
+        );
     });
     const len = dataRef.current.length;
     const lastRow = dataRef.current[len - 1];
@@ -232,6 +279,13 @@ export default function ReferenceTable({
           text={t.deleteConfirm.replace('{0}', confirmMsg(confirmRow))}
           yesResponse={deleteConfirmed}
           noResponse={deleteRejected}
+        />
+      )}
+      {confirmErr && (
+        <Confirm
+          text={t.errorData}
+          yesResponse={saveConfirm}
+          noResponse={saveRejected}
         />
       )}
     </Stack>
