239d2af81a70ec68957c3e5ff111f827c6b50b8b
Greg Trihus <greg_trihus@sil.org>
2023-04-26 09:29:39 -0500

    TT-5068 Only move discussions not comments

--- src/components/Discussions/DiscussionMenu.tsx
+++ src/components/Discussions/DiscussionMenu.tsx
@@ -122,14 +122,16 @@ export function DiscussionMenu(props: IProps) {
             <ListItemText primary={t.setSegment} />
           </StyledMenuItem>
         )}
-        <StyledMenuItem
-          id="move"
-          aria-hidden={!Boolean(anchorEl)}
-          onClick={handle('move')}
-        >
-          <ListItemIcon></ListItemIcon>
-          <ListItemText primary={t.move} />
-        </StyledMenuItem>
+        {resolved !== undefined && (
+          <StyledMenuItem
+            id="move"
+            aria-hidden={!Boolean(anchorEl)}
+            onClick={handle('move')}
+          >
+            <ListItemIcon></ListItemIcon>
+            <ListItemText primary={t.move} />
+          </StyledMenuItem>
+        )}
       </StyledMenu>
     </div>
   );
