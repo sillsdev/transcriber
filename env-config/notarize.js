51e3d9644d22a56680442dc779287b2fce9a1f34
gtryus <gtryus@gmail.com>
2023-08-18 16:06:41 -0500

    update for Mac not signing and notarizing

--- env-config/notarize.js
+++ env-config/notarize.js
@@ -3,18 +3,18 @@ const { notarize } = require('@electron/notarize');
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
-  console.log(`platform ${electronPlatformName}`)
-  if (electronPlatformName !== 'darwin') {
+  // Notarizing worked for a previous version but doesn't work now.
+  if (electronPlatformName !== 'darwin' || true) {
     return;
   }
 
   const appName = context.packager.appInfo.productFilename;
-  console.log(`notarizing ${appName}`)
+  console.log(`  \u2022 notarizing ${appName}`)
 
   return await notarize({
     appBundleId: 'sil.lsdev.audio-project-manager',
     appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
+    appleId: `${process.env.APPLEID}`,
+    appleIdPassword: `${process.env.APPLEIDPASS}`,
   });
 };
