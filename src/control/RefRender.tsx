a297037f5c52ddd4447f05fe00550bc050daec58
Greg Trihus <greg_trihus@sil.org>
2023-09-18 08:05:14 -0500

    Refactor RefRender

--- src/control/RefRender.tsx
+++ src/control/RefRender.tsx
@@ -19,16 +19,23 @@ export const isPublishingTitle = (ref?: string, flat?: boolean) =>
   passageTypeFromRef(ref, flat) !== PassageTypeEnum.PASSAGE &&
   passageTypeFromRef(ref, flat) !== PassageTypeEnum.NOTE;
 
-const typeAndArg = (val: string, len: number, icon: JSX.Element) =>
-  val.length > len + 1 ? (
+interface AtProps {
+  value: any;
+  type: PassageTypeEnum;
+  icon: JSX.Element;
+}
+const ArgType = ({ value, type, icon }: AtProps) => {
+  const len = type.length;
+  const val = value.toString();
+  if (val.length <= len + 1) return icon;
+  return (
     <>
       {icon}
       {`\u00A0`}
       {val.substring(len)}
     </>
-  ) : (
-    icon
   );
+};
 
 /**
  * Determines the passage type based on the input value and returns the
@@ -44,11 +51,7 @@ export const refRender = (value: any, flat: boolean) => {
     case PassageTypeEnum.MOVEMENT:
       return MovementIcon;
     case PassageTypeEnum.CHAPTERNUMBER:
-      return typeAndArg(
-        value.toString(),
-        PassageTypeEnum.CHAPTERNUMBER.length,
-        ChapterNumberIcon
-      );
+      return <ArgType value type={pt} icon={ChapterNumberIcon} />;
     case PassageTypeEnum.TITLE:
       return TitleIcon;
     case PassageTypeEnum.BOOK:
@@ -56,11 +59,7 @@ export const refRender = (value: any, flat: boolean) => {
     case PassageTypeEnum.ALTBOOK:
       return AltBookIcon;
     case PassageTypeEnum.NOTE:
-      return typeAndArg(
-        value.toString(),
-        PassageTypeEnum.NOTE.length,
-        NoteIcon
-      );
+      return <ArgType value type={pt} icon={NoteIcon} />;
     default:
       return value;
   }
