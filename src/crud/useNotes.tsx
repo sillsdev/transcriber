4e6d384bcfffea41a0757357235f1046603a8d45
gtryus <greg_trihus@sil.org>
2024-02-08 11:29:08 -0600

    TT-5643 Only Team Linked Notes (#1512)
    Co-authored-by: Greg Trihus <gtryus@gmail.com>

--- src/crud/useNotes.tsx
+++ src/crud/useNotes.tsx
@@ -1,13 +1,22 @@
 import { useGlobal } from 'reactn';
-import { SharedResourceD } from '../model';
+import { PassageD, PlanD, ProjectD, SectionD, SharedResourceD } from '../model';
+import related from './related';
+import { useOrbitData } from '../hoc/useOrbitData';
+import { findRecord } from '.';
 
 export const useNotes = () => {
+  const sharedResources = useOrbitData<SharedResourceD[]>('sharedresource');
   const [memory] = useGlobal('memory');
+  const [organization] = useGlobal('organization');
 
   return () => {
-    const sharedResources = memory.cache.query((q) =>
-      q.findRecords('sharedresource')
-    ) as SharedResourceD[];
-    return sharedResources.filter((sr) => sr?.attributes?.note ?? false);
+    return sharedResources.filter((sr) => {
+      const passRec = findRecord(memory, 'passage', related(sr, 'passage')) as PassageD[]
+      const secRec = findRecord(memory, 'section', related(passRec, 'section')) as SectionD[]
+      const planRec = findRecord(memory, 'plan', related(secRec, 'plan')) as PlanD[]
+      const projRec = findRecord(memory, 'project', related(planRec, 'project')) as ProjectD[]
+      if (related(projRec, 'organization') !== organization) return false
+      return sr?.attributes?.note ?? false
+    });
   };
 };
