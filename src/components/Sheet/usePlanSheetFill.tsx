5e3c930694f59b6b27110e9396dd1e625c2159b6
gtryus <greg_trihus@sil.org>
2024-02-09 17:15:47 -0600

    TT-5652 Use Publish Button control (#1516)

--- src/components/Sheet/usePlanSheetFill.tsx
+++ src/components/Sheet/usePlanSheetFill.tsx
@@ -9,8 +9,8 @@ import { ICell, ICellChange } from './PlanSheet';
 import { planSheetSelector, viewModeSelector } from '../../selector';
 import { related, useOrganizedBy, useRole } from '../../crud';
 import { rowTypes } from './rowTypes';
-import { LightTooltip, StageReport } from '../../control';
-import { Avatar, Badge, IconButton } from '@mui/material';
+import { StageReport } from '../../control';
+import { Avatar, Badge } from '@mui/material';
 import PlanAudioActions from './PlanAudioActions';
 import {
   RefRender,
@@ -31,8 +31,7 @@ import { ExtraIcon } from '.';
 import { positiveWholeOnly, stringAvatar } from '../../utils';
 import { TitleEdit } from './TitleEdit';
 import { getPubRefs } from './getPubRefs';
-import { PublishIcon } from '../../control/PlanIcons';
-import RadioButtonUnchecked from '@mui/icons-material/RadioButtonUnchecked';
+import { PublishButton } from './PublishButton';
 
 type ICellEditor = (props: any) => JSX.Element;
 type IRow = (string | number)[];
@@ -291,35 +290,14 @@ export const usePlanSheetFill = ({
   }: ActionValueProps) => {
     const isShowIcon = showIcon(filtered, offline && !offlineOnly, rowIndex);
     if (isShowIcon(ExtraIcon.Publish)) {
-      const sectionSequenceNumber =
-        sectionMap.get(rowInfo[rowIndex].sectionSeq) ||
-        positiveWholeOnly(rowInfo[rowIndex].sectionSeq);
-      return rowInfo[rowIndex].published ? (
-        <LightTooltip
-          title={t.unpublish
-            .replace('{0}', organizedBy)
-            .replace('{1}', sectionSequenceNumber)}
-        >
-          <IconButton
-            id="unpublish"
-            onClick={() => onAction(rowIndex, ExtraIcon.Publish)}
-          >
-            <PublishIcon />
-          </IconButton>
-        </LightTooltip>
-      ) : (
-        <LightTooltip
-          title={t.publish
-            .replace('{0}', organizedBy)
-            .replace('{1}', sectionSequenceNumber)}
-        >
-          <IconButton
-            id="publish"
-            onClick={() => onAction(rowIndex, ExtraIcon.Publish)}
-          >
-            <RadioButtonUnchecked color='primary'/>
-          </IconButton>
-        </LightTooltip>
+      return (
+        <PublishButton
+          sectionMap={sectionMap}
+          rowInfo={rowInfo}
+          rowIndex={rowIndex}
+          organizedBy={organizedBy}
+          onAction={onAction}
+        />
       );
     }
     if (!passage) return <></>;
