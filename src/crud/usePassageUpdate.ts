214c0cdb2d2b271d667b62401ab71401cee551d4
Sara Hentzel <sara_hentzel@sil.org>
2024-02-28 20:35:29 -0600

    put it back

--- src/crud/usePassageUpdate.ts
+++ src/crud/usePassageUpdate.ts
@@ -2,18 +2,15 @@ import { useGlobal } from 'reactn';
 import { PassageD } from '../model';
 import { RecordTransformBuilder } from '@orbit/records';
 import { ReplaceRelatedRecord, UpdateRecord } from '../model/baseModel';
-import { PassageTypeEnum } from '../model/passageType';
-import { usePassageType } from './usePassageType';
 
 export const usePassageUpdate = () => {
   const [memory] = useGlobal('memory');
   const [user] = useGlobal('user');
-  const { getPassageTypeRec } = usePassageType();
 
   return async (
     passage: PassageD,
     sectionId?: string,
-    passageType?: PassageTypeEnum,
+    passageType?: string,
     sharedResourceId?: string
   ) => {
     const t = new RecordTransformBuilder();
@@ -23,18 +20,16 @@ export const usePassageUpdate = () => {
         ...ReplaceRelatedRecord(t, passage, 'section', 'section', sectionId)
       );
     }
-    if (passageType !== undefined && passageType !== PassageTypeEnum.PASSAGE) {
-      var pt = getPassageTypeRec(passageType);
-      if (pt)
-        ops.push(
-          ...ReplaceRelatedRecord(
-            t,
-            passage,
-            'passagetype',
-            'passagetype',
-            pt.id
-          )
-        );
+    if (passageType !== undefined) {
+      ops.push(
+        ...ReplaceRelatedRecord(
+          t,
+          passage,
+          'passagetype',
+          'passagetype',
+          passageType
+        )
+      );
     }
     if (sharedResourceId) {
       ops.push(
