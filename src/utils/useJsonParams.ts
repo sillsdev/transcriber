5f18e8fbd28a125ce450ccb8bf61cef1c5f86ded
sarahentzel <47537281+sarahentzel@users.noreply.github.com>
2024-03-28 21:28:44 -0500

    Database serializes json with space so check value not serialized string (#1575)

--- src/utils/useJsonParams.ts
+++ src/utils/useJsonParams.ts
@@ -9,8 +9,31 @@ export const useJsonParams = () => {
 
   const setParam = (label: string, value: any, params: string | undefined) => {
     const json = JSON.parse(params ?? '{}');
-    json[label] = JSON.stringify(value);
+    if (value !== undefined) {
+      var tmp = JSON.stringify(value);
+      if (tmp !== json[label]) {
+        json[label] = tmp;
+      }
+    } else if ((json[label] ?? '') !== '') {
+      delete json[label];
+    }
     return JSON.stringify(json);
   };
-  return { getParam, setParam };
+  const willSetParam = (
+    label: string,
+    value: any,
+    params: string | undefined
+  ) => {
+    const json = JSON.parse(params ?? '{}');
+    if (value !== undefined) {
+      var tmp = JSON.stringify(value);
+      if (tmp !== json[label]) {
+        return true;
+      }
+    } else if ((json[label] ?? '') !== '') {
+      return true;
+    }
+    return false;
+  };
+  return { getParam, setParam, willSetParam };
 };
