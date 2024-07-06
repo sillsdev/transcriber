b3b436b8f1077662d6e717465da022f36f939bac
Greg Trihus <gtryus@gmail.com>
2024-07-06 13:17:39 -0500

    TT-5947 Improve prettySegment to handle empty

--- src/utils/prettySegment.ts
+++ src/utils/prettySegment.ts
@@ -7,16 +7,16 @@ export const prettySegment = (region: IRegion | undefined | string) => {
   if (typeof region === 'string') {
     if (region) rgn = JSON.parse(region) as IRegion;
   } else rgn = region;
-  if (rgn) return `${onePlace(rgn.start)}-${onePlace(rgn.end)} `;
+  if (rgn?.start !== undefined)
+    return `${onePlace(rgn.start)}-${onePlace(rgn.end)} `;
   return '';
 };
-
 
 export const prettySegmentStart = (region: IRegion | undefined | string) => {
   var rgn: IRegion | undefined = undefined;
   if (typeof region === 'string') {
     if (region) rgn = JSON.parse(region) as IRegion;
   } else rgn = region;
-  if (rgn) return onePlace(rgn.start).padStart(8,' ');
+  if (rgn) return onePlace(rgn.start).padStart(8, ' ');
   return '';
 };
