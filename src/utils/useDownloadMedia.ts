4238f3a57eb6e1b189c92c5aa60e3227cee1adf4
sarahentzel <47537281+sarahentzel@users.noreply.github.com>
2024-08-05 16:57:32 -0500

    TT-4786b Fix error when downloading from desktop (#1692)
    * TT-4786 check file was downloaded
    
    * TT-4786b move safeurl to useFetchMediaUrl

--- src/utils/useDownloadMedia.ts
+++ src/utils/useDownloadMedia.ts
@@ -3,14 +3,6 @@ import path from 'path-browserify';
 const ipc = (window as any)?.electron;
 
 export const useDownloadMedia = () => {
-  const safeURL = async (path: string) => {
-    if (!path.startsWith('http')) {
-      const start = (await ipc?.isWindows()) ? 8 : 7;
-      const url = new URL(`file://${path}`).toString().slice(start);
-      return `transcribe-safe://${url}`;
-    }
-    return path;
-  };
   const tryDownload = async (url: string, safe: boolean) => {
     var local = { localname: '' };
     var where = await dataPath(url, PathType.MEDIA, local);
@@ -18,9 +10,10 @@ export const useDownloadMedia = () => {
     if (where !== local.localname) {
       try {
         ipc?.createFolder(path.dirname(local.localname));
+        console.log('downloading', local.localname, url);
         await ipc?.downloadFile(url, local.localname);
         if (await ipc?.exists(local.localname)) {
-          if (safe) return await safeURL(local.localname);
+          if (safe) return local.localname;
           return local.localname;
         } else return url;
       } catch (e: any) {
@@ -29,5 +22,5 @@ export const useDownloadMedia = () => {
     }
     return local.localname;
   };
-  return { tryDownload, safeURL };
+  return { tryDownload };
 };
