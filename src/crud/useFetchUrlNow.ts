dd53c601def0eaef8b7c897cbb9e7c76f21009eb
sarahentzel <47537281+sarahentzel@users.noreply.github.com>
2024-08-16 13:02:04 -0500

    TT-5995 catch 401 error on fileurl call (#1703)
    * TT-5995 catch 401 error on fileurl call

--- src/crud/useFetchUrlNow.ts
+++ src/crud/useFetchUrlNow.ts
@@ -5,6 +5,9 @@ import { API_CONFIG, isElectron } from '../api-variable';
 import { TokenContext } from '../context/TokenProvider';
 
 import { useDownloadMedia } from '../utils';
+import { useSelector, shallowEqual } from 'react-redux';
+import { ISharedStrings } from '../model';
+import { sharedSelector } from '../selector';
 
 export interface IFetchNowProps {
   id: string;
@@ -13,23 +16,30 @@ export interface IFetchNowProps {
 export const useFetchUrlNow = () => {
   const { accessToken } = useContext(TokenContext).state;
   const { tryDownload } = useDownloadMedia();
+  const ts: ISharedStrings = useSelector(sharedSelector, shallowEqual);
 
   const fetchUrl = async (props: IFetchNowProps) => {
     let { id, cancelled } = props;
-    var strings = await Axios.get(
-      `${API_CONFIG.host}/api/mediafiles/${id}/fileurl`,
-      {
-        headers: {
-          Authorization: 'Bearer ' + accessToken,
-        },
-      }
-    );
-    const attr: any = strings.data.data.attributes;
-    if (!attr || cancelled()) return;
-    const audioUrl = attr['audio-url'] as string;
-    if (isElectron) {
-      return await tryDownload(audioUrl, true);
-    } else return audioUrl;
+    try {
+      var strings = await Axios.get(
+        `${API_CONFIG.host}/api/mediafiles/${id}/fileurl`,
+        {
+          headers: {
+            Authorization: 'Bearer ' + accessToken,
+          },
+        }
+      );
+      const attr: any = strings.data.data.attributes;
+      if (!attr || cancelled()) return;
+      const audioUrl = attr['audio-url'] as string;
+      if (isElectron) {
+        return await tryDownload(audioUrl, true);
+      } else return audioUrl;
+    } catch (error: any) {
+      if (error.errStatus === 401) return ts.expiredToken;
+      throw error;
+    }
   };
+
   return fetchUrl;
 };
