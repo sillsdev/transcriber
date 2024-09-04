e2368d0d04b7fc5e7ba9277a163f01fc0bf2f54f
Greg Trihus <gtryus@gmail.com>
2024-09-04 13:07:17 -0500

    TT-6045 TT-5995 Handle Audacity 3.6.x
    - new keys in settings file needed for integration

--- src/utils/setAudacityPref.ts
+++ src/utils/setAudacityPref.ts
@@ -47,6 +47,14 @@ const getRecordChannels = (content: string) => {
   return /RecordChannels=([12])/.exec(content);
 };
 
+const getSaveLocation = (content: string) => {
+  return /SaveLocationMode=([a-zA-Z0-9]*)/.exec(content);
+};
+
+const getExpLocation = (content: string) => {
+  return /ExportLocationMode=([a-zA-Z0-9]*)/.exec(content);
+};
+
 const getTmpMatch = (content: string) => {
   return /\[Directories\]\r?\nTempDir=([^\r\n]*)\r?\n/.exec(content || '');
 };
@@ -59,6 +67,12 @@ const getDirMatch = (content: string) => {
 
 const getIoMatch = (content: string) => {
   return /\[Directories\/(Import|Export)\]\r?\nDefault=([^\r\n]*)/.exec(
+    content || ''
+  );
+};
+
+const getExpMatch = (content: string) => {
+  return /\[ExportAudioDialog\]\r?\nFormat=([A-Z1-3]*)\r?\nDefaultPath=([^\r\n]*)/.exec(
     content || ''
   );
 };
@@ -77,7 +91,27 @@ const changeValueTo1 = (data: string, m: RegExpExecArray) => {
   return data.slice(0, pos) + '1' + data.slice(pos + 1);
 };
 
+const changeSaveMode = (
+  data: string,
+  m: RegExpExecArray | null,
+  val: string
+) => {
+  // set the save location Mode to local
+  const cloudAudio = /\[cloud\/audiocom[^\n]*\r?\n([^[\r\n]*\r?\n)*/.exec(data);
+  if (cloudAudio) {
+    const start = m ? m.index : cloudAudio.index + cloudAudio[0].length;
+    const end = m ? m.index + m[0].length : start;
+    data =
+      data.slice(0, start) +
+      val +
+      (start === end ? '\r\n' : '') +
+      data.slice(end);
+  }
+  return data;
+};
+
 export const setAudacityPref = async (fullName: string) => {
+  // create folders for audio projects and import/export
   let folder = path.dirname(fullName);
   await ipc?.createFolder(folder);
   let ioFolder = folder.replace('aup3', 'io');
@@ -87,30 +121,71 @@ export const setAudacityPref = async (fullName: string) => {
     ioFolder = ioFolder.replace(/\\/g, `\\\\`);
   }
 
+  // get the current audacity.cfg content
   const prefName = (await audPrefsName()) || '';
   const beforeContent = (await getAudPrefContent(prefName)) || '';
+
+  // check if the folders are already set
   const save = getDirMatch(beforeContent);
   const saveFolder = (save && save[2]) || '';
   const imp = getIoMatch(beforeContent);
   const impFolder = (imp && imp[2]) || '';
-  if (saveFolder === folder && impFolder === ioFolder) return false;
+  const exp = getExpMatch(beforeContent);
+  const expFolder = (exp && exp[2]) || '';
+  if (saveFolder === folder && impFolder === ioFolder && expFolder === ioFolder)
+    return false;
+
+  // remove the directories section
   let cleanContent = beforeContent.replace(
     /\[Directories\/[^\n]*\r?\n([^[\r\n]*\r?\n)*/g,
     ''
   );
+
+  // setRecording channels to 1
   const channels = getRecordChannels(cleanContent);
   if (channels) cleanContent = changeValueTo1(cleanContent, channels);
   // We expect the audacity.cfg to always contain the recording channels
-  const tmp = getTmpMatch(beforeContent);
+
+  // set the save location Mode to local
+  cleanContent = changeSaveMode(
+    cleanContent,
+    getSaveLocation(cleanContent),
+    'SaveLocationMode=local'
+  );
+  cleanContent = changeSaveMode(
+    cleanContent,
+    getExpLocation(cleanContent),
+    'ExportLocationMode=local'
+  );
+
+  // set the folders
+  const tmp = getTmpMatch(cleanContent);
   let pos = cleanContent.length;
   if (tmp) pos = cleanContent.indexOf(tmp[0]) + tmp[0].length;
-  const passContent =
+  let passContent =
     cleanContent.slice(0, pos) +
     (await setFolder('Save', folder)) +
     (await setFolder('Open', folder)) +
     (await setFolder('Import', ioFolder)) +
     (await setFolder('Export', ioFolder)) +
     cleanContent.slice(pos);
+
+  // Replace Export path
+  const expMatch = getExpMatch(passContent);
+  if (expMatch) {
+    const expPos =
+      passContent.indexOf(expMatch[0]) +
+      expMatch[0].length -
+      expMatch[2].length;
+    passContent =
+      passContent.slice(0, expPos) +
+      ioFolder +
+      passContent.slice(expPos + expMatch[2].length);
+  } else {
+    passContent += `[ExportAudioDialog]\r\nFormat=wav\r\nDefaultPath=${ioFolder}\r\nExportRange=project\r\n`;
+  }
+
+  // write the new content
   resetAudacityPref(prefName, passContent);
   return beforeContent;
 };
