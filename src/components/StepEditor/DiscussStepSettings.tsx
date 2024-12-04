a28d2efb83ebb97e872086133c2d7f865048e2e6
Greg Trihus <gtryus@gmail.com>
2024-12-04 12:56:21 -0600

    TT-4967, TT-6191 Discuss tool save and cancel buttons
    - save closes as well
    - cancel closes without saving
    - close closes when there is nothing to save

--- src/components/StepEditor/DiscussStepSettings.tsx
+++ src/components/StepEditor/DiscussStepSettings.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { MarkDownEdit } from '../../control/MarkDownEdit';
 import { Box } from '@mui/material';
-import { ActionRow, PriButton } from '../../control';
+import { ActionRow, AltButton, PriButton } from '../../control';
 import { shallowEqual, useSelector } from 'react-redux';
 import { sharedSelector } from '../../selector';
 import { ISharedStrings } from '../../model';
@@ -9,9 +9,15 @@ import { ISharedStrings } from '../../model';
 interface IProps {
   toolSettings: string;
   onChange: (toolSettings: string) => void;
+  onClose: () => void;
 }
 
-export const DiscussStepSettings = ({ toolSettings, onChange }: IProps) => {
+export const DiscussStepSettings = ({
+  toolSettings,
+  onChange,
+  onClose,
+}: IProps) => {
+  const [inValue, setInValue] = useState<string>('');
   const [value, setValue] = useState<string>('');
   const ts: ISharedStrings = useSelector(sharedSelector, shallowEqual);
 
@@ -19,6 +25,7 @@ export const DiscussStepSettings = ({ toolSettings, onChange }: IProps) => {
     if (toolSettings) {
       var json = JSON.parse(toolSettings);
       setValue(json.markDown);
+      setInValue(json.markDown);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [toolSettings]);
@@ -29,13 +36,19 @@ export const DiscussStepSettings = ({ toolSettings, onChange }: IProps) => {
 
   const handleSave = () => {
     onChange(JSON.stringify({ markDown: value }));
+    onClose();
   };
 
   return (
     <Box>
       <MarkDownEdit inValue={value} onValue={handleChange} />
       <ActionRow>
-        <PriButton onClick={handleSave}>{ts.save}</PriButton>
+        <AltButton onClick={onClose}>
+          {value === inValue ? ts.close : ts.cancel}
+        </AltButton>
+        <PriButton onClick={handleSave} disabled={value === inValue}>
+          {ts.save}
+        </PriButton>
       </ActionRow>
     </Box>
   );
