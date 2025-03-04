0d5085f36409dfd59a7d101fcec9220b0744678d
ratatozk <elijahlewis@cedarville.edu>
2025-03-04 12:33:23 -0700

    Create ProfileDialog.test.tsx

---
+++ src/components/ProfileDialog.test.tsx
@@ -0,0 +1,5 @@
+/* eslint-disable testing-library/no-node-access */
+/* eslint-disable testing-library/no-container */
+// See: https://www.w3schools.com/TAGS/ref_av_dom.asp
+import { cleanup, render, waitFor } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
