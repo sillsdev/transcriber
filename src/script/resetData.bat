fc13f7c4212351ad0ca263feb45758aa9f011b6b
Greg Trihus <gtryus@gmail.com>
2025-04-07 14:20:57 -0500

    reset data script update

--- src/script/resetData.bat
+++ src/script/resetData.bat
@@ -1,2 +1,2 @@
-powershell rm -Force -Recurse %APPDATA%\audio-project-manager
-powershell Invoke-Item "%APPDATA%\..\Local\Programs\audio-project-manager\SIL` Transcriber` Desktop.exe"
+powershell rm -Force -Recurse %APPDATA%\audio-project-manager-training
+powershell Invoke-Item "%APPDATA%\..\Local\Programs\audio-project-manager-training\Audio` Project` Manager` Training.exe"
