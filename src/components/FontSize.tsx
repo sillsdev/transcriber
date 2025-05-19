49cd2825c10a35884cfd8dbc8b625420651ffbde
gtryus <gtrihus59@yahoo.com>
2025-05-19 17:11:26 -0500

    TT-1522 setting line height by size (#1950)

--- src/components/FontSize.tsx
+++ src/components/FontSize.tsx
@@ -24,6 +24,15 @@ const fontSizes = [
   'xx-large',
 ];
 
+export const fontSpace = [
+  ['xx-small', 8],
+  ['x-small', 10],
+  ['small', 12],
+  ['medium', 14],
+  ['large', 16],
+  ['x-large', 18],
+  ['xx-large', 20],
+];
 interface IProps {
   label?: string;
   value?: string;
